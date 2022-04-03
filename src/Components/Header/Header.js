2cb9f2f31c0c73dbef5051362df6e3ef46e5b126
sraboni-bhuiyan <bhuiyansraboni@gmail.com>
2022-04-04 01:46:01 +0200

    data and header added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='bg-teal-200'>
+            <div className='w-1/2 mx-auto sticky top-0 flex justify-around gap-7 text-xl font-semibold px-12 py-8'>
+                <Link to='/'>Home</Link>
+                <Link to='/reviews'>Reviews</Link>
+                <Link to='/dashboard'>Dashboard</Link>
+                <Link to='/blog'>Blog</Link>
+                <Link to='/about'>About</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
